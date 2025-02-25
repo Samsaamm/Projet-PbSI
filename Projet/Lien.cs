48ef50b405b10bd5664404175b466b82de0be6be
Percio <sarah.percio@edu.devinci.fr>
2025-02-25 16:59:30 +0100

    Classes

--- Projet/Lien.cs
+++ Projet/Lien.cs
@@ -8,5 +8,41 @@ namespace Projet
 {
     internal class Lien
     {
+        private Noeud depart;
+        private Noeud arrivee;
+
+        /// <summary>
+        /// Constructeur de la classe
+        /// </summary>
+        /// <param name="depart">noeud de départ</param>
+        /// <param name="arrivee">noeud d'arrivée</param>
+        public Lien (Noeud depart, Noeud arrivee)
+        {
+            this.depart = depart;
+            this.arrivee = arrivee;
+        }
+
+        public Noeud Depart
+        {
+            get { return depart; }
+            set { this.depart = value; }
+        }
+
+        public Noeud Arrivee
+        {
+            get { return arrivee; }
+            set { arrivee = value; }
+        }
+
+        /// <summary>
+        /// Affichage des caractéristiques d'un lien
+        /// </summary>
+        /// <returns>le début et la fin du lien</returns>
+
+        public string toString()
+        {
+            string s = "Lien : Départ du noeud : " + depart.Numnoeud + " jusqu'au noeud " + arrivee.Numnoeud;
+            return s;
+        }
     }
 }
--- Projet/Noeud.cs
+++ Projet/Noeud.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Projet
 {
-    internal class Noeud
+    public class Noeud
     {
+        private int numnoeud;
+        private List<Noeud> voisins;
+
+        /// <summary>
+        /// Constructeur de la classe
+        /// </summary>
+        /// <param name="numnoeud">indique le numéro du noeud</param>
+        public Noeud (int numnoeud)
+        {
+            this.numnoeud = numnoeud;
+            this.voisins = new List<Noeud>();
+        }
+
+
+        public int Numnoeud
+        {
+            get { return numnoeud; }
+            set { this.numnoeud = value; }
+        }
+
+        public List<Noeud> Voisins
+        {
+            get { return voisins; }
+            set { voisins = value; }
+        }
+
+        /// <summary>
+        /// Affiche les caractéristiques d'un noeud
+        /// </summary>
+        /// <returns>les caractéristiques d'un noeud avec sa liste de voisins</returns>
+        public string toString()
+        {
+            string s = "Noeud numéro : " + numnoeud + "\nliste des noeuds voisins : ";
+            foreach (Noeud n in voisins)
+            {
+                s += n.ToString() + "\n";
+            }
+            return s;
+        }
+
+
     }
 }

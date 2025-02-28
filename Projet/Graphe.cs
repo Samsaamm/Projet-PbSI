03eb55c23e15df2a61ad0fdfa84b49577015be0c
Samsam <samuel.philip2005@gmail.com>
2025-02-28 12:44:36 +0100

    Class Graphe

--- Projet/Graphe.cs
+++ Projet/Graphe.cs
@@ -8,5 +8,46 @@ namespace Projet
 {
     internal class Graphe
     {
+        public string titre;
+        public bool IsOriented;
+        public List<Noeud> noeuds;
+        public List<Lien> liens;
+        public Dictionary<Noeud, List<Noeud>> mat_adj;
+
+        /// <summary>Constructeur du graphe manuel</summary>
+        public Graphe(string titre, bool oriented, List<Noeud> noeuds, List<Lien> liens){
+            this.titre = titre;
+            this.IsOriented = oriented;
+            this.noeuds = noeuds;
+            this.liens = liens;
+        }
+
+        /// <summary>Constructeur du graphe a partir d'un fichier source</summary>
+        /// <param name="filepath">String, Indique le chemin vers le fichier a ouvrir</param>
+        public Graphe(string filepath){
+            try{
+
+            }catch(exception E){
+        
+            }finally{
+
+            }
+        }
+
+        public Dictionary<Noeud, List<Noeud>> Matrice_adj(){
+            
+        }
+
+        public void DFS(){
+
+        }
+
+        public void BFS(){
+            
+        }
+
+        public void Show(){
+
+        }
     }
 }
